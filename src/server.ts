470a2454df0cfb0ef85259ad7d0bc29296d0fe50
dani voro <mr.voro@gmail.com>
2023-09-19 16:03:33 +0200

    reducing innecessary stuff

--- src/server.ts
+++ src/server.ts
@@ -1,42 +1,50 @@
-import express from "express";
-const { createCanvas, loadImage, registerFont } = require("canvas");
-import path from "path";
+const express = require("express");
+const { createCanvas } = require("canvas");
 
 const app = express();
 const PORT = 3000;
 
-const fontPath = "./data/LeagueGothic-Regular.otf";
-registerFont(fontPath, { family: "CustomFont" });
+app.get(
+  "/",
+  async (
+    req: { query: { text: string } },
+    res: {
+      set: (arg0: string, arg1: string) => void;
+      status: (arg0: number) => {
+        (): any;
+        new (): any;
+        send: { (arg0: string): void; new (): any };
+      };
+    }
+  ) => {
+    const canvas = createCanvas(800, 600);
+    const ctx = canvas.getContext("2d");
 
-app.get("/", async (req, res) => {
-  const canvas = createCanvas(800, 600);
-  const ctx = canvas.getContext("2d");
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#f0f0f0";
+    ctx.font = "80px Arial";
 
-  ctx.fillStyle = "#f0f0f0";
-  ctx.font = "80px CustomFont";
-  const text = "Resource image not found for:";
+    const text = "Image not found for:";
+    const userText = req.query.text || "";
 
-  const textWidth = ctx.measureText(text).width;
-  const x = (canvas.width - textWidth) / 2;
-  const y = canvas.height / 2;
+    const textWidth = ctx.measureText(text).width;
+    const x = (canvas.width - textWidth) / 2;
+    const y = canvas.height / 2;
 
-  const userText = req.query.text || "";
+    ctx.fillText(text, x, y - 50);
+    ctx.fillText(userText, x + 170, y + 50);
 
-  ctx.fillText(text, x, y - 50);
-  ctx.fillText(userText, x + 170, y + 50);
-
-  try {
-    res.set("Content-Type", "image/jpeg");
-
-    canvas.createJPEGStream().pipe(res);
-  } catch (error) {
-    console.error("Error loading or drawing the logo:", error);
-    res.status(500).send("An error occurred while generating the image.");
+    try {
+      res.set("Content-Type", "image/jpeg");
+      canvas.createJPEGStream().pipe(res);
+    } catch (error) {
+      console.error("Error loading or drawing the image:", error);
+      res.status(500).send("An error occurred while generating the image.");
+    }
   }
-});
+);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
